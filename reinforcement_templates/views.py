5b7d58c6bc767dcb137cdbd5003a21a220ae5186
Vikil Naik <vikilnaik37@gmail.com>
2019-03-05 13:51:33 -0500

    Finished excercise

--- reinforcement_templates/views.py
+++ reinforcement_templates/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def new_profile(request):
-    context = {}
+    context = {'forms': ['email', 'username', 'pin', 'website', 'address', 'alias']}
     return HttpResponse(render(request, 'profiles/new.html', context))
